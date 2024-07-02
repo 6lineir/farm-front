ea4c2bb1bf8c1e9bbdceb28ca232f26e9976ffd6
6lineir <ali.baghaeiheravi77@gmail.com>
2024-07-02 19:07:19 +0330

    Fix UI

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -62,7 +62,7 @@ const AppLayout = () => {
                         </div>
                         <button data-tooltip-target="tooltip-wallet" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                             <Link to="/tasks">
-                                <img src="/public/icon1/gift-box.svg" />
+                                <img src="/icon1/gift-box.svg" />
                                 <span className="sr-only">Tasks</span>
                             </Link>
                         </button>
@@ -73,7 +73,7 @@ const AppLayout = () => {
                         <div className="flex items-center justify-center">
                             <button data-tooltip-target="tooltip-new" type="button" className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
                                 <Link to="/">
-                                    <img src="/public/icon1/coin.svg" className="w-[30px]" />
+                                    <img src="/icon1/coin.svg" className="w-[30px]" />
                                     <span className="sr-only">Home</span>
                                 </Link>
                             </button>
@@ -84,7 +84,7 @@ const AppLayout = () => {
                         </div>
                         <button data-tooltip-target="tooltip-settings" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                             <Link to='/frend'>
-                                <img src="/public/icon1/users.svg" />
+                                <img src="/icon1/users.svg" />
                                 <span className="sr-only">Frends</span>
                             </Link>
                         </button>
@@ -94,7 +94,7 @@ const AppLayout = () => {
                         </div>
                         <button data-tooltip-target="tooltip-profile" type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
                             <Link to="/airdrop" >
-                                <img src="/public/icon1/airdrop.svg" />
+                                <img src="/icon1/airdrop.svg" />
                                 {/* <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
                             </svg> */}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,62 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useTimer } from 'react-timer-hook';
+
+function MyTimer({ expiryTimestamp }) {
+    const {
+        totalSeconds,
+        seconds,
+        minutes,
+        hours,
+        days,
+        isRunning,
+        start,
+        pause,
+        resume,
+        restart,
+    } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
+
+    const ClaimHandler = () => {
+        if (hours == 0 && minutes == 0 && seconds == 0) {
+            console.log(minutes);
+            return true
+
+        } else return false
+
+    }
+    // ClaimHandler()
 
 
+    return (
+        <div className='inline-flex justify-between items-center mr-3 '>
 
+            <span disabled hidden={ClaimHandler() === false} className="text-xs  bg-blue-500 rounded-full text-white px-4 py-1.5 me-3" onClick={() => {
+                // Restarts to 5 minutes timer
+                const time = new Date();
+                time.setSeconds(time.getSeconds() + 480);
+                restart(time)
+            }}>
+                Claim
+            </span>
+            <div className='pl-2 dark:text-blue-300 justify-center items-center align-center' style={{ textAlign: 'center' }}>
+                <div >
+                    <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+                </div>
+                {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
+
+            </div>
+        </div>
+    );
+}
 const HomePage = () => {
+    const Ltime = localStorage.getItem('timem')
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 480)
+
+    // console.log(time);
+
     return (
         <div className="h-[89.89dvh] bg-gradient-to-r from-sky-800 to-indigo-500 bg-cover bg-center  text-center justify-center ">
+
             <div className="mx-3 p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 rounded-full">
                 <p className="dark:text-gray-100">Time Level: 1</p>
             </div>
@@ -18,7 +71,12 @@ const HomePage = () => {
             </div>
             <div className="mt-12">
                 <div href="#" className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
-                    <span className="text-xs bg-gray-500 rounded-full text-white px-4 py-1.5 me-3">Claim</span> <span className="text-sm font-medium">7:35:10 | Farm 0.0045</span>
+
+
+                    <MyTimer expiryTimestamp={time} />
+
+
+                    <span className="text-sm font-medium">Farm 0.0045</span>
 
                 </div>
             </div>
